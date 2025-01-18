5129719a538904b0efdb0771eb213610ecad1557
salihsenturk97 <salihsenturk97@gmail.com>
2025-01-18 14:43:45 +0300

    Feign Client add

--- AuthMicroservice/src/main/java/com/salihsenturk/service/AuthService.java
+++ AuthMicroservice/src/main/java/com/salihsenturk/service/AuthService.java
@@ -1,8 +1,10 @@
 package com.salihsenturk.service;
 
+import com.salihsenturk.dto.request.CreateUserRequestDto;
 import com.salihsenturk.dto.request.RegisterRequestDto;
 import com.salihsenturk.dto.response.LoginResponseDto;
 import com.salihsenturk.entity.Auth;
+import com.salihsenturk.manager.UserProfileManager;
 import com.salihsenturk.repository.AuthRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,13 +13,21 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AuthService {
     private final AuthRepository authRepository;
+    private final UserProfileManager userProfileManager;
 
     public Auth register(RegisterRequestDto dto) {
-        return authRepository.save(Auth.builder()
+        Auth auth = authRepository.save(Auth.builder()
                 .userName(dto.getUserName())
                 .email(dto.getEmail())
                 .password(dto.getPassword())
                 .build());
+
+        userProfileManager.createUser(CreateUserRequestDto.builder()
+                .username(auth.getUserName())
+                .authId(auth.getId())
+                .email(auth.getEmail())
+                .build());
+        return auth;
     }
 
     public Boolean login(LoginResponseDto dto) {
